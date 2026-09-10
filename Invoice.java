fbc98642f7d8a83a1bb276259eb1b814cb44cf42
Ajay <7448200+Ajay90@users.noreply.github.com>
2026-09-10 12:40:33 +0530

    Create Invoice.java
    Single Responsibility Principle - ill maintained class.

---
+++ Invoice.java
@@ -0,0 +1,14 @@
+public class Invoice{
+  
+  double calculateTotal(){
+    // Calculate total value of items on this invoice.
+  }
+
+  void storeInvoiceDetails(){
+    // Persist the invoice to a database.
+  }
+
+  void printInvoice(){
+    // Print the invoice in a specific format
+  }
+}
