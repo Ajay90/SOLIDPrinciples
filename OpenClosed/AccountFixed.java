2f8723ee63f9361b6a4638f84badd4b57bd29ee8
Ajay <7448200+Ajay90@users.noreply.github.com>
2026-09-10 13:01:45 +0530

    Create AccountFixed.java
    Open/Closed Princple - God class separated into meaningful and extensible class

---
+++ OpenClosed/AccountFixed.java
@@ -0,0 +1,7 @@
+public class AccountFixed{ // Fields and accessors omitted.
+
+  public double calculateInterest(InterestCalculation interestCalculation){
+    return interestCalculation.calculateInterest(balance);
+  }
+  
+}
